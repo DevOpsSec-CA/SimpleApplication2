064ea5ec2176a79808f23063e4b0512cdabfac13
michaele490 <michaelegharevba490@gmail.com>
2025-03-19 18:10:24 +0000

    Added deployment automation

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+sudo apt update && sudo apt install nodejs npm
+# Install pm2 (process manager for Node.js)
+sudo npm install -g pm2
+# Stop runnings instances if necessary
+pm2 stop simple_app
+# Cd into application directory
+cd SimpleApplication/
+# Install app dependencies
+npm install
+# Collects private key and certificate values from repository secrets
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+# Set environment to production
+export ENV=PROD
+# Start the application using pm2 under name simple_app
+pm2 start ./bin/www --name simple_app
